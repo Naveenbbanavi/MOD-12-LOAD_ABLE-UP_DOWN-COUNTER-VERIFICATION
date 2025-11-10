97003f2c889cc1eedd939dbdadfa5b2cc6d3614c
Naveenbbanavi <naveenbanavi031@gmail.com>
2025-11-10 20:06:06 +0530

    rd_xtn.sv

---
+++ rd_agt_top/rd_xtn.sv
@@ -0,0 +1,10 @@
+class read_xtn extends uvm_sequence_item;
+ `uvm_object_utils(read_xtn)
+
+bit[3:0]data_out;
+ function new(string name = "read_xtn");
+ super.new(name);
+endfunction:new
+
+
+endclass
