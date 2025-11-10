c7dc27e3a8340be3af23a664d0ea278a0014b9a5
Naveenbbanavi <naveenbanavi031@gmail.com>
2025-11-10 22:33:12 +0530

    wr_driver.sv

--- wr_agt_top/wr_driver.sv
+++ wr_agt_top/wr_driver.sv
@@ -1,52 +1,55 @@
-class write_monitor extends uvm_monitor;
-`uvm_component_utils(write_monitor)
-
-virtual counter_if.WR_MON_MP vif;
-
+class write_driver extends uvm_driver #(write_xtn);
+`uvm_component_utils(write_driver)
 write_agent_config m_cfg;
-uvm_analysis_port#(write_xtn) monitor_port;
-function new(string name = "write_monitor", uvm_component parent);
-super.new(name,parent);
-monitor_port=new("monitor_port",this);
+
+virtual counter_if.WR_DRV_MP vif;
+
+function new(string name ="write_driver",uvm_component parent);
+	super.new(name,parent);
 endfunction
 
 
 function void build_phase(uvm_phase phase);
 super.build_phase(phase);
-uvm_config_db#(write_agent_config)::get(this,"","write_agent_config",m_cfg);
+if(!uvm_config_db#(write_agent_config)::get(this,"","write_agent_config",m_cfg))
+ `uvm_fatal("CONFG","getting config failed")
 endfunction
 
-
 function void connect_phase(uvm_phase phase);
-super.connect_phase(phase);
 vif=m_cfg.vif;
 endfunction
 
 task run_phase(uvm_phase phase);
-//	`uvm_info(get_type_name(),"THIS IS MONITOR IN RUN",UVM_LOW)
-	forever
-            collect_data();   
-endtask
+super.run_phase(phase);
+  // `uvm_info(get_type_name(), "This is Driver run phase", UVM_MEDIUM)
+  @(vif.wr_drv_cb);
+  vif.wr_drv_cb.rst<=1;
+  @(vif.wr_drv_cb);
+  //@(vif.wr_drv_cb);
+  vif.wr_drv_cb.rst<=0;
+ //   @(vif.wr_drv_cb);
 
-task collect_data();
-begin
-write_xtn data_send;
-data_send=write_xtn::type_id::create("data_send");
-     wait(vif.wr_mon_cb.rst==0);
+  
+  forever 
+   begin
+	  seq_item_port.get_next_item(req);
+	  send_to_dut(req);
+	  seq_item_port.item_done();
+   end
+endtask 
 
-       data_send.rst =  vif.wr_mon_cb.rst;
-        data_send.load_in= vif.wr_mon_cb.load_in;
-        data_send.up_down =  vif.wr_mon_cb.up_down;
-        data_send.data_in= vif.wr_mon_cb.data_in;
+task send_to_dut(write_xtn xtn);
+  
+//repeat(2)
+   // @(vif.wr_drv_cb);
 
+	 vif.wr_drv_cb.data_in<=xtn.data_in;
+	 vif.wr_drv_cb.load_in<=xtn.load_in;
+	 vif.wr_drv_cb.up_down<=xtn.up_down;
+	 
+   // @(vif.wr_drv_cb);
+ `uvm_info("WR_DRIVER",$sformatf("printing from driver \n %s", xtn.sprint()),UVM_LOW) 
+  @(vif.wr_drv_cb);
 
-`uvm_info("WR_MONITOR",$sformatf("printing from monitor \n %s", data_send.sprint()),UVM_LOW);
-monitor_port.write(data_send);
- @(vif.wr_mon_cb);
- 
-end
-
-endtask
-
-	
-endclass 
+	endtask
+ endclass
